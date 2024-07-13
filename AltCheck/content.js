c1884cc79c985e7581542e8e19bb771a3141d61c
FiddllePat <89714380+FiddllePat@users.noreply.github.com>
2024-07-13 16:58:21 +1000

    Update content.js

--- AltCheck/content.js
+++ AltCheck/content.js
@@ -8,7 +8,7 @@ if (window.location.host === "www.roblox.com") {
     
         if (userId) {
             console.log(`User ID found: ${userId}`);
-            const apiUrl = `https://alt-new-model.fiddllepat.com/predict?user_id=${encodeURIComponent(userId)}`;
+            const apiUrl = `https://altmodel.fiddllep.at/predict?user_id=${encodeURIComponent(userId)}`;
             fetch(apiUrl)
                 .then(response => response.json())
                 .then(data => {
@@ -21,7 +21,7 @@ if (window.location.host === "www.roblox.com") {
                         const highestCategory = Object.keys(categories).reduce((a, b) => categories[a] > categories[b] ? a : b);
                         const highestPercentage = categories[highestCategory];
                         addNewListItem(highestCategory, highestPercentage);
-                        insertBadgeActivityImage(userId);
+                        //insertBadgeActivityImage(userId);
                     } else {
                         console.error('Error fetching data:', data);
                     }
@@ -43,93 +43,102 @@ if (window.location.host === "www.roblox.com") {
         }
     }      
 
-function insertBadgeActivityImage(userId) {
-    const imageUrl = `https://altcheck-demo.fiddllepat.com/graph_badge_activity/${userId}`;
-    const footerDiv = document.querySelector('.border-top.profile-about-footer');
-    if (footerDiv) {
-        const textLabel = document.createElement('span');
-        textLabel.textContent = 'Badge Activity';
-        textLabel.style.display = 'block';
-        textLabel.style.fontWeight = 'bold';
-        textLabel.style.marginBottom = '5px';
+    function insertBadgeActivityImage(userId) {
+        const imageUrl = `https://altcheck-demo.fiddllepat.com/graph_badge_activity/${userId}`;
+        const footerDiv = document.querySelector('.border-top.profile-about-footer');
+        if (footerDiv) {
+            const textLabel = document.createElement('span');
+            textLabel.textContent = 'Badge Activity';
+            textLabel.style.display = 'block';
+            textLabel.style.fontWeight = 'bold';
+            textLabel.style.marginBottom = '5px';
 
-        const loadingText = document.createElement('span');
-        loadingText.textContent = 'Loading';
-        loadingText.style.display = 'block';
-        footerDiv.appendChild(loadingText);
+            const loadingText = document.createElement('span');
+            loadingText.textContent = 'Loading';
+            loadingText.style.display = 'block';
+            footerDiv.appendChild(loadingText);
 
-        let dotCount = 0;
-        const loadingInterval = setInterval(() => {
-            loadingText.textContent = `Loading${'.'.repeat(dotCount)}`;
-            dotCount = (dotCount + 1) % 4;
-        }, 500);
+            let dotCount = 0;
+            const loadingInterval = setInterval(() => {
+                loadingText.textContent = `Loading${'.'.repeat(dotCount)}`;
+                dotCount = (dotCount + 1) % 4;
+            }, 500);
 
-        const img = document.createElement('img');
-        img.src = imageUrl;
-        img.alt = 'Badge Activity';
-        img.style.maxWidth = '100%';
-        img.style.height = 'auto';
-        img.style.display = 'none';
+            const img = document.createElement('img');
+            img.src = imageUrl;
+            img.alt = 'Badge Activity';
+            img.style.maxWidth = '100%';
+            img.style.height = 'auto';
+            img.style.display = 'none';
 
-        img.onload = () => {
-            clearInterval(loadingInterval);
-            loadingText.remove();
-            img.style.display = 'block';
-        };
+            img.onload = () => {
+                clearInterval(loadingInterval);
+                loadingText.remove();
+                img.style.display = 'block';
+            };
 
-        if (footerDiv.firstChild) {
-            footerDiv.insertBefore(textLabel, footerDiv.firstChild);
-            footerDiv.insertBefore(img, textLabel.nextSibling);
-        } else {
-            footerDiv.appendChild(textLabel);
-            footerDiv.appendChild(img);
+            if (footerDiv.firstChild) {
+                footerDiv.insertBefore(textLabel, footerDiv.firstChild);
+                footerDiv.insertBefore(img, textLabel.nextSibling);
+            } else {
+                footerDiv.appendChild(textLabel);
+                footerDiv.appendChild(img);
+            }
+
+            console.log("started loading the badge activity");
         }
+    }
 
-        console.log("started loading the badge activity");
+    function fetchLastOnline(userId) {
+        const lastOnlineApiUrl = 'https://presence.roblox.com/v1/presence/last-online';
+        fetch(lastOnlineApiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'accept': 'application/json'
+            },
+            body: JSON.stringify({
+                "userIds": [parseInt(userId)]
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+            const lastOnlineTimestamps = data.lastOnlineTimestamps;
+            if (lastOnlineTimestamps && lastOnlineTimestamps.length > 0) {
+                const lastOnline = lastOnlineTimestamps[0].lastOnline;
+                const lastOnlineDate = new Date(lastOnline);
+                const daysAgo = Math.floor((new Date() - lastOnlineDate) / (1000 * 60 * 60 * 24));
+                let lastOnlineText;
+                if (daysAgo === 0) {
+                    lastOnlineText = 'Last Online Today';
+                } else if (daysAgo === 1) {
+                    lastOnlineText = 'Last Online Yesterday';
+                } else {
+                    lastOnlineText = `Last Online ${daysAgo} Days Ago`;
+                }
+
+                const profileDisplayName = document.querySelector('.profile-display-name.font-caption-body.text.text-overflow');
+                if (profileDisplayName) {
+                    const originalText = profileDisplayName.textContent;
+                    profileDisplayName.innerHTML = `${originalText} | ${lastOnlineText}`;
+                }
+            }
+        })
+        .catch(error => console.error('error fetching last online data:', error));
+    }
+
+    if (document.readyState === "loading") {
+        document.addEventListener('DOMContentLoaded', fetchAndDisplayAlt);
+    } else {
+        fetchAndDisplayAlt();
     }
 }
 
-function fetchLastOnline(userId) {
-  const lastOnlineApiUrl = 'https://presence.roblox.com/v1/presence/last-online';
-  fetch(lastOnlineApiUrl, {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json',
-          'accept': 'application/json'
-      },
-      body: JSON.stringify({
-          "userIds": [parseInt(userId)]
-      })
-  })
-  .then(response => response.json())
-  .then(data => {
-      const lastOnlineTimestamps = data.lastOnlineTimestamps;
-      if (lastOnlineTimestamps && lastOnlineTimestamps.length > 0) {
-          const lastOnline = lastOnlineTimestamps[0].lastOnline;
-          const lastOnlineDate = new Date(lastOnline);
-          const daysAgo = Math.floor((new Date() - lastOnlineDate) / (1000 * 60 * 60 * 24));
-          let lastOnlineText;
-          if (daysAgo === 0) {
-              lastOnlineText = 'Last Online Today';
-          } else if (daysAgo === 1) {
-              lastOnlineText = 'Last Online Yesterday';
-          } else {
-              lastOnlineText = `Last Online ${daysAgo} Days Ago`;
-          }
-
-          const profileDisplayName = document.querySelector('.profile-display-name.font-caption-body.text.text-overflow');
-          if (profileDisplayName) {
-              const originalText = profileDisplayName.textContent;
-              profileDisplayName.innerHTML = `${originalText} | ${lastOnlineText}`;
-          }
-      }
-  })
-  .catch(error => console.error('error fetching last online data:', error));
-}
-
-if (document.readyState === "loading") {
-  document.addEventListener('DOMContentLoaded', fetchAndDisplayAlt);
-} else {
-  fetchAndDisplayAlt();
-}
+if (window.location.href === "https://www.roblox.com/home") {
+    const homeContainer = document.querySelector('#HomeContainer.row.home-container.expand-max-width');
+    if (homeContainer) {
+        homeContainer.classList.remove('expand-max-width');
+        homeContainer.style.maxWidth = '60%';
+        console.log("Updated HomeContainer max-width to 60%");
+    }
 }
